486e95e542400a615ed6379b5abe0b48e805ac86
Cang Nguyen Duc <cang_nduc@live.com>
2025-02-28 23:04:53 +0700

    init

---
+++ src/lib/auth.ts
@@ -0,0 +1,11 @@
+import { betterAuth } from "better-auth";
+import prisma from "./prisma";
+
+export const auth = betterAuth({
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+});
